df949ef45e515cfcd95b59a8c525b03738782375
LOKESH E <lokesh.karthike@gmail.com>
2025-07-05 17:49:22 +0530

    Created modular frontend components without React

--- client/script.js
+++ client/script.js
@@ -1,1 +1,11 @@
-console.log("Frontend Initialized");
+import { createDonorCard } from './components/donorCard.js';
+
+const donorList = document.getElementById('donorList');
+
+const sampleDonor = {
+  id: "123",
+  name: "Lokesh",
+  bloodGroup: "O+"
+};
+
+donorList.innerHTML = createDonorCard(sampleDonor);
